87b87ede5c959ecc2dc8f7742504f5c55f164792
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-09-19 22:45:35 +0800

    feat:delete class and material

--- src/app/classes/[class_id]/material/[material_id]/page.tsx
+++ src/app/classes/[class_id]/material/[material_id]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Trash, Info, Pencil } from "lucide-react";
 
 // component
 import {
@@ -13,6 +13,17 @@ import {
   DialogFooter,
   DialogClose,
 } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog";
 import {
   Card,
   CardHeader,
@@ -60,8 +71,139 @@ export default async function page({ params }: Props) {
           - Detail Material {material_id} -
         </h1>
 
+        <div className="mx-auto mb-3 md:w-2/3">
+          <Card className="border-primary bg-primary/10 border">
+            <CardFooter className="justify-between gap-2">
+              <Button
+                variant="destructive"
+                className="text-destructive hover:bg-destructive border-destructive border bg-white hover:text-white"
+                asChild
+              >
+                <Link href={"/classes/1"}>
+                  <ArrowLeft />
+                  Back
+                </Link>
+              </Button>
+
+              <div className="flex gap-3">
+                {/* Delete Material */}
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="destructive">
+                      <Trash />
+                      Delete
+                    </Button>
+                  </AlertDialogTrigger>
+
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Delete Material</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Are you sure?
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+
+                    <AlertDialogFooter>
+                      <AlertDialogCancel asChild>
+                        <Button
+                          variant="destructive"
+                          className="text-destructive hover:bg-destructive border-destructive border bg-white hover:text-white"
+                        >
+                          Cancel
+                        </Button>
+                      </AlertDialogCancel>
+
+                      <AlertDialogAction asChild>
+                        <Button
+                          variant="destructive"
+                          className="text-destructive border-destructive !bg-destructive hover:!bg-destructive/90 border"
+                        >
+                          <Trash /> Delete
+                        </Button>
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+
+                {/* View Quiz */}
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button className="border-primary text-primary hover:bg-primary cursor-pointer border bg-white hover:text-white">
+                      <Info /> View Quiz
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent className="h-fit min-w-3/4">
+                    <DialogHeader>
+                      <DialogTitle>Questions</DialogTitle>
+                      <DialogDescription>Preview Questions</DialogDescription>
+                    </DialogHeader>
+
+                    <ScrollArea className="border-primary h-96 w-full rounded-xl border p-5">
+                      <div className="flex flex-col gap-2">
+                        {Questions.map((question, indexQ) => (
+                          <Card
+                            className="border-primary bg-primary/10 gap-0 border"
+                            key={indexQ}
+                          >
+                            <CardHeader>
+                              <CardTitle className="text-2xl">
+                                {indexQ + 1}.
+                              </CardTitle>
+                            </CardHeader>
+
+                            <CardContent>{question.question_text}</CardContent>
+
+                            <CardFooter className="mt-5">
+                              <RadioGroup>
+                                {question.answers.map((answer, indexA) => (
+                                  <div
+                                    className="flex items-center space-x-2"
+                                    key={indexA}
+                                  >
+                                    <RadioGroupItem
+                                      value={answer}
+                                      id={`${indexQ}-option-${indexA}`}
+                                      className="border-primary border"
+                                      disabled
+                                    />
+                                    <Label
+                                      htmlFor={`${indexQ}-option-${indexA}`}
+                                      className="text-md font-normal"
+                                    >
+                                      {answer}
+                                    </Label>
+                                  </div>
+                                ))}
+                              </RadioGroup>
+                            </CardFooter>
+                          </Card>
+                        ))}
+                      </div>
+                    </ScrollArea>
+
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button className="border-primary text-primary border bg-white hover:text-white">
+                          Close
+                        </Button>
+                      </DialogClose>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+
+                <Button asChild>
+                  <Link href={`/classes/${class_id}/quiz/${material_id}`}>
+                    <Pencil /> Take the Quiz
+                  </Link>
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
+
         <div className="mx-auto md:w-2/3">
-          <Card>
+          <Card className="border-primary border">
             <CardContent>
               <h3 className="text-start text-xl font-bold">Material</h3>
 
@@ -80,92 +222,6 @@ export default async function page({ params }: Props) {
                 labore reprehenderit a. Deserunt libero quis at deleniti.
               </p>
             </CardContent>
-
-            <CardFooter className="justify-end gap-2">
-              <Button asChild>
-                <Button
-                  variant="destructive"
-                  className="text-destructive hover:bg-destructive border-destructive border bg-white hover:text-white"
-                  asChild
-                >
-                  <Link href={"/classes/1"}>
-                    <ArrowLeft />
-                    Back
-                  </Link>
-                </Button>
-              </Button>
-
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button className="border-primary text-primary hover:bg-primary cursor-pointer border bg-white hover:text-white">
-                    View Quiz
-                  </Button>
-                </DialogTrigger>
-
-                <DialogContent className="h-fit min-w-3/4">
-                  <DialogHeader>
-                    <DialogTitle>Questions</DialogTitle>
-                    <DialogDescription>Preview Questions</DialogDescription>
-                  </DialogHeader>
-
-                  <ScrollArea className="border-primary h-96 w-full rounded-xl border p-5">
-                    <div className="flex flex-col gap-2">
-                      {Questions.map((question, indexQ) => (
-                        <Card
-                          className="border-primary bg-primary/10 gap-0 border"
-                          key={indexQ}
-                        >
-                          <CardHeader>
-                            <CardTitle className="text-2xl">
-                              {indexQ + 1}.
-                            </CardTitle>
-                          </CardHeader>
-
-                          <CardContent>{question.question_text}</CardContent>
-
-                          <CardFooter className="mt-5">
-                            <RadioGroup>
-                              {question.answers.map((answer, indexA) => (
-                                <div
-                                  className="flex items-center space-x-2"
-                                  key={indexA}
-                                >
-                                  <RadioGroupItem
-                                    value={answer}
-                                    id={`${indexQ}-option-${indexA}`}
-                                    className="border-primary border"
-                                  />
-                                  <Label
-                                    htmlFor={`${indexQ}-option-${indexA}`}
-                                    className="text-md font-normal"
-                                  >
-                                    {answer}
-                                  </Label>
-                                </div>
-                              ))}
-                            </RadioGroup>
-                          </CardFooter>
-                        </Card>
-                      ))}
-                    </div>
-                  </ScrollArea>
-
-                  <DialogFooter>
-                    <DialogClose asChild>
-                      <Button className="border-primary text-primary border bg-white hover:text-white">
-                        Close
-                      </Button>
-                    </DialogClose>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-
-              <Button asChild>
-                <Link href={`/classes/${class_id}/quiz/${material_id}`}>
-                  Take the Quiz
-                </Link>
-              </Button>
-            </CardFooter>
           </Card>
         </div>
       </section>
--- src/app/classes/[class_id]/page.tsx
+++ src/app/classes/[class_id]/page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { Plus, ArrowLeft, Trash } from "lucide-react";
 
 // component
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 type Props = {
@@ -19,6 +36,62 @@ export default async function page({ params }: Props) {
     <>
       <section className="container mb-10 px-10">
         <h1 className="my-5 text-center text-4xl font-bold">- Class Name -</h1>
+
+        <div className="mb-5">
+          <Card className="border-primary bg-primary/10 border">
+            <CardFooter className="gap-3">
+              <Button
+                variant="destructive"
+                className="text-destructive hover:bg-destructive border-destructive border bg-white hover:text-white"
+                asChild
+              >
+                <Link href={"/classes"}>
+                  <ArrowLeft />
+                  Back
+                </Link>
+              </Button>
+
+              {/* Delete Material */}
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="destructive">
+                    <Trash />
+                    Delete
+                  </Button>
+                </AlertDialogTrigger>
+
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Delete Class</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Are you sure?
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+
+                  <AlertDialogFooter>
+                    <AlertDialogCancel asChild>
+                      <Button
+                        variant="destructive"
+                        className="text-destructive hover:bg-destructive border-destructive border bg-white hover:text-white"
+                      >
+                        Cancel
+                      </Button>
+                    </AlertDialogCancel>
+
+                    <AlertDialogAction asChild>
+                      <Button
+                        variant="destructive"
+                        className="text-destructive border-destructive !bg-destructive hover:!bg-destructive/90 border"
+                      >
+                        <Trash /> Delete
+                      </Button>
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </CardFooter>
+          </Card>
+        </div>
 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           <Link href={`/classes/${class_id}/material/1`}>
